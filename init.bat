65f17e4e0171644232687d844c26dfedf7497153
Cewein <pro.mnowak@gmail.com>
2019-08-22 22:06:17 +0200

    update init.bat
    better init and remove git clone of Logrash since it's a submodules now

--- init.bat
+++ init.bat
@@ -1,7 +1,8 @@
+git submodule init
+git submodule update
 if exist Dependencies rmdir /s /Q Dependencies
 mkdir Dependencies
 cd Dependencies
-git clone https://github.com/Cewein/Logarsh
 if exist dependencies.h del dependencies.h
 ( echo [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 && echo.Invoke-WebRequest https://github.com/glfw/glfw/releases/download/3.3/glfw-3.3.bin.WIN32.zip -OutFile glfw-3.3.zip) > glfw.ps1
 Powershell.exe -executionpolicy remotesigned -File glfw.ps1
@@ -14,5 +15,5 @@ move glfw-3.3.bin.WIN32\lib-vc2017 .\
 move glfw-3.3.bin.WIN32\LICENSE.md .\
 rename lib-vc2017 lib
 rmdir /s /Q glfw-3.3.bin.WIN32
-cd ..
+cd ../../
 (echo #pragma once && echo.#include "Logrash/logrash.h" && echo.#include ^<GLFW/glfw3.h^>) > dependencies.h
