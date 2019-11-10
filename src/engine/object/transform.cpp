d27c40fefe7b329e8e9b73e0e9c9bfdff94b9270
Cewein <pro.mnowak@gmail.com>
2019-11-10 23:08:32 +0100

    different approch in transform.cpp
    transform work perfecly but the aproch is different!

--- src/engine/object/transform.cpp
+++ src/engine/object/transform.cpp
@@ -3,30 +3,54 @@
 nerv::transform::transform()
 {
 	this->trans = glm::mat4(1.0);
+	this->positionVec = glm::vec3(0.);
+	this->scaleVec = glm::vec3(1.);
+	this->rotateVec = glm::vec3(0.);
 }
 
 void nerv::transform::reset()
 {
+	this->positionVec = glm::vec3(0.);
+	this->scaleVec = glm::vec3(1.);
+	this->rotateVec = glm::vec3(0.);
 	this->trans = glm::mat4(1.0);
 }
 
 void nerv::transform::translate(glm::vec3 move)
 {
-	this->trans = glm::translate(this->trans, move);
+	this->positionVec = move;
+
 }
 
 void nerv::transform::rotate(double angle, glm::vec3 axes)
 {
-	this->trans = glm::rotate(this->trans, (float)glm::radians(angle), axes);
+	this->rotateVec += (float)glm::radians(angle) * axes;
 }
 
 void nerv::transform::scale(glm::vec3 scale)
 {
-	this->trans = glm::scale(this->trans, scale);
+	this->scaleVec *=  scale;
+}
+
+void nerv::transform::rotateX(double angle)
+{
+}
+
+void nerv::transform::rotateY(double angle)
+{
+}
+
+void nerv::transform::rotateZ(double angle)
+{
 }
 
 glm::mat4x4 nerv::transform::getTransformMatrix()
 {
+	this->trans = glm::translate(this->trans, this->positionVec);
+	this->trans = glm::rotate(this->trans, this->rotateVec.x, glm::vec3(1, 0, 0));
+	this->trans = glm::rotate(this->trans, this->rotateVec.y, glm::vec3(0, 1, 0));
+	this->trans = glm::rotate(this->trans, this->rotateVec.z, glm::vec3(0, 0, 1));
+	this->trans = glm::scale(this->trans, this->scaleVec);
 	return this->trans;
 }
 
--- src/engine/object/transform.h
+++ src/engine/object/transform.h
@@ -8,6 +8,11 @@ namespace nerv
 	{
 	private:
 
+		//info for the set transform matrix
+		glm::vec3 positionVec;
+		glm::vec3 scaleVec;
+		glm::vec3 rotateVec;
+
 		//mat4 who containe all the info of the object,
 		//she is reset ever time show() is used
 		glm::mat4x4 trans;
@@ -20,6 +25,11 @@ namespace nerv
 		void translate(glm::vec3 move);
 		void rotate(double angle, glm::vec3 axes);
 		void scale(glm::vec3 scale);
+
+		//rotation on one axe
+		void rotateX(double angle);
+		void rotateY(double angle);
+		void rotateZ(double angle);
 
 		//get transform matrices
 		glm::mat4x4 getTransformMatrix();
