068b20024e158b5d36dfe7e5fce5867e249b6e56
Cewein <pro.mnowak@gmail.com>
2020-05-11 01:27:02 +0200

    move data generation in function

--- main.cpp
+++ main.cpp
@@ -37,35 +37,10 @@ int main()
 	nerv::camera * cam = new nerv::camera(nerv::camera::projectionType::PERSPECTIVE_PROJECTION, 90.0f);
 	cam->transform->translate(glm::vec3(0., 0., 3.));
 
-	std::uniform_real_distribution<float_t> floatGen(0.0f,1.0f);
-
-	const int size = 500;
-
-	sphere * s = new sphere[size];
+	const int size = 6;
+	sphere * s = makeSphereData(size);
 	
-	for(int i = 0; i < size - 1; i++)
-	{ 
-		float x = floatGen(rng) * 80.0f - 40.0f;
-		float y = floatGen(rng) * 80.0f - 40.0f;
-		float z = floatGen(rng) * 80.0f - 40.0f;
-		float sz = 0.5f;
-
-		s[i] =
-		{
-			{x, 0.f, z, 1.0f},
-			{sz, floorf(floatGen(rng) * 3.f), floatGen(rng), floatGen(rng) + 1.f},
-			{floatGen(rng),floatGen(rng), floatGen(rng), 0.0f}
-		};
-	}
-
-	s[size - 1] =
-	{
-		{0.0f, -2000.5f, 0.0f, 1.0f},
-		{2000.0f, 0.f, 0.f, 0.f},
-		{0.4f,0.4f, 0.4f, 0.0f}
-	};
-
-	int nbSample = 2;
+	int nbSample = 10;
 	
 	unsigned int ssbo = nerv::shader::createBuffer(sizeof(sphere) * size, s);
 
@@ -98,7 +73,7 @@ int main()
 			ImGui::Text("Camera");
 			ImGui::Separator();
 			ImGui::SliderFloat("FOV", &(cam->fov), 0.0, 180.0);
-			ImGui::SliderFloat("focus Distance", &(cam->focusDistance), 0.0001, 20.0);
+			ImGui::SliderFloat("focus Distance", &(cam->focusDistance), 0.0001, 50.0);
 			ImGui::SliderFloat("apperture", &(cam->aperture), 0.0, 1.0);
 
 			ImGui::Text("rendering");
@@ -124,4 +99,36 @@ int main()
 
 	nerv::window::get().close();
 	return EXIT_SUCCESS;
+}
+
+sphere * makeSphereData(int size)
+{
+
+	std::uniform_real_distribution<float_t> floatGen(0.0f, 1.0f);
+
+	sphere * s = new sphere[size];
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		float x = floatGen(rng) * 80.0f - 40.0f;
+		float y = floatGen(rng) * 80.0f - 40.0f;
+		float z = floatGen(rng) * 80.0f - 40.0f;
+		float sz = 0.5f;
+
+		s[i] =
+		{
+			{x, 0.f, z, 1.0f},
+			{sz, floorf(floatGen(rng) * 3.f), floatGen(rng), floatGen(rng) + 1.f},
+			{floatGen(rng),floatGen(rng), floatGen(rng), 0.0f}
+		};
+	}
+
+	s[size - 1] =
+	{
+		{0.0f, -2000.5f, 0.0f, 1.0f},
+		{2000.0f, 0.f, 0.f, 0.f},
+		{0.4f,0.4f, 0.4f, 0.0f}
+	};
+
+	return s;
 }
